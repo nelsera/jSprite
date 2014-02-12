88b24a81810cf27cad75be29de48a3cff79a17f3
Nelson Francisco <nelsondelega@gmail.com>
2014-02-12 11:01:49 -0200

    npm modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,35 @@
 'use strict';
 
 module.exports = function (grunt) {
-    grunt.initConfig({
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
 
+    grunt.initConfig({
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        'dist'
+                    ]
+                }]
+            }
+        },
+
+        copy: {
+            files: [{
+                expand: true,
+                dot: true,
+                cwd: 'source',
+                dest: 'dist',
+                src: [
+                    'source/{,*/}*.*'
+                ]
+            }]
+        }
     });
 
     grunt.registerTask('dist', function () {
-
+        'clean:dist'
     });
 };
